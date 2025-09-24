d94947a5c9d0b69ee012f0f7f1277ff979eb5856
kaareltiitt6nts <kaarel-tiit.tonts@voco.ee>
2025-09-24 06:18:28 +0000

    kasutaja registreerimine

--- controller/userController.js
+++ controller/userController.js
@@ -5,28 +5,36 @@ const userModel = new UserModel()
 
 class UserController {
     async register(req, res) {
-        const bcryptPass = bcrypt.hash(req.body.password, 10)
+        try {
+            const bcryptPass = await bcrypt.hash(req.body.password, 10)
 
-        const registeredId = userModel.create({
-            username: req.params.username,
-            password: bcryptPass,
-            email: req.params.email
-        }) 
+            const registeredId = await userModel.create({
+                username: req.body.username,
+                password: bcryptPass,
+                email: req.body.email
+            }) 
 
-        if (registeredId) {
-            const userData = userModel.findById(registeredId)
+            if (registeredId) {
+                const userData = await userModel.findById(registeredId)
 
-            req.session.user = {
-                username: userData.username,
-                id: userData.id
+                req.session.user = {
+                    username: userData.username,
+                    id: userData.id
+                }
+
+                res.json({
+                    message: "Register success",
+                    user_session: req.session.user
+                })
             }
+            else {
+                res.json({
+                    message: "Failed to register user"
+                })
+            }
+        } catch (error) {
+            console.log(error)
 
-            res.json({
-                message: "Register success",
-                user_session: req.session.user
-            })
-        }
-        else {
             res.json({
                 message: "Failed to register user"
             })
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,6 @@ import UserController from "../controller/userController.js"
 const userRouter = express.Router()
 const userController = new UserController()
 
-userRouter.post("/users/register", (req, res) => userController.register(req, res))
+userRouter.post("/user/register", (req, res) => userController.register(req, res))
 
 export default userRouter
